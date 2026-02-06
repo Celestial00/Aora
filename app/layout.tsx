593567544eeed7cfb4a4077ac672089b049b793a
Celestial00 <sheerazalee223@gmail.com>
2026-02-06 21:20:55 +0500

    minor font change'

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import Navbar from "./components/main/Navbar";
 import Footer from "./components/main/Footer";
 import "./globals.css";
 import CustomCursor from "./components/main/CustomeCursor";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Aora",
@@ -16,8 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col bg-zinc-900  from-indigo-500/10 via-transparent to-cyan-500/10">
-        <CustomCursor />  
+      <body
+        className={` ${poppins.className} min-h-screen flex flex-col bg-zinc-900  from-indigo-500/10 via-transparent to-cyan-500/10`}
+      >
+        <CustomCursor />
         <main className="flex-1 px-10 py-5 md:px-30 md:py-5">
           <Navbar />
           {children}
